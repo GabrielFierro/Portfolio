5329e7e70311dae7c5052bfadfd4dcf6726ccc16
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-02 20:46:54 -0300

    add: about me component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Header from './components/header/Header';
+import AboutMe from './components/about/AboutMe';
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <AboutMe />
     </>
   );
 }
