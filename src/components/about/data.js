0717d6e6ec1fdd42803e7daa2f712681aaed2f0e
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-02 20:45:10 -0300

    add: an array with the information about the icons with his name, alt, and path

---
+++ src/components/about/data.js
@@ -0,0 +1,66 @@
+import HTML from '../../assets/icons/light-mode/brand-light/html5.svg';
+import CSS from '../../assets/icons/light-mode/brand-light/css3-simple.svg';
+import Javascript from '../../assets/icons/light-mode/brand-light/javascript.svg';
+import Git from '../../assets/icons/light-mode/brand-light/git.svg';
+import Github from '../../assets/icons/light-mode/brand-light/github.svg';
+import React from '../../assets/icons/light-mode/brand-light/react.svg';
+import Bootstrap from '../../assets/icons/light-mode/brand-light/bootstrap.svg';
+import Tailwind from '../../assets/icons/light-mode/brand-light/tailwindcss.svg';
+import ChakraUI from '../../assets/icons/light-mode/brand-light/chakraui.svg';
+
+export const data = [
+  {
+    id: 1,
+    src: HTML,
+    alt: 'HTML icon',
+    name: 'HTML'
+  },
+  {
+    id: 2,
+    src: CSS,
+    alt: 'CSS icon',
+    name: 'CSS'
+  },
+  {
+    id: 3,
+    src: Javascript,
+    alt: 'Javascript icon',
+    name: 'Javascript'
+  },
+  {
+    id: 4,
+    src: Git,
+    alt: 'Git icon',
+    name: 'Git'
+  },
+  {
+    id: 5,
+    src: Github,
+    alt: 'Github icon',
+    name: 'Github'
+  },
+  {
+    id: 6,
+    src: React,
+    alt: 'React icon',
+    name: 'React'
+  },
+  {
+    id: 7,
+    src: Bootstrap,
+    alt: 'Bootstrap icon',
+    name: 'Bootstrap'
+  },
+  {
+    id: 8,
+    src: Tailwind,
+    alt: 'Tailwind icon',
+    name: 'Tailwind'
+  },
+  {
+    id: 9,
+    src: ChakraUI,
+    alt: 'ChakraUI icon',
+    name: 'ChakraUI'
+  }
+];
