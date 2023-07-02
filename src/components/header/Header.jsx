eb4f7f1925cc2ef82095023825efbcc3cc12504b
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-02 20:18:10 -0300

    add: styles for the texts and the button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import ProfilePicture from '../../assets/images/profile-picture.webp';
 import GithubIcon from '../../assets/icons/github-alt.svg';
 import LinkedInIcon from '../../assets/icons/linkedin-alt.svg';
+import ArrowAlt from '../../assets/icons/dark-mode/arrow-alt.svg';
 
 export default function Header() {
   return (
@@ -34,10 +35,27 @@ export default function Header() {
             />
           </li>
         </ul>
-        <h1>Hi, I&apos;m Gabriel Fierro</h1>
-        <h3>Frontend Developer</h3>
-        <p>Specialized in CSS, Javascript, and React js based in Argentina</p>
-        <button>Contact Me</button>
+        <section className='text-left  w-auto h-52'>
+          <h1 className='text-title text-2xl font-bold pb-1'>
+            Hi, I&apos;m Gabriel Fierro
+          </h1>
+          <h3 className='text-description text-lg font-normal pb-3'>
+            Frontend Developer
+          </h3>
+          <p className='text-description text-base font-light pb-4'>
+            Specialized in CSS, Javascript, and React js based in Argentina.
+          </p>
+          <button className='bg-accent text-lightMode font-light flex space-x-2 items-center p-3 rounded'>
+            <p>Contact Me</p>
+            <img
+              className='rotate-90'
+              src={ArrowAlt}
+              alt='Arrow right'
+              width='24px'
+              height='24px'
+            />
+          </button>
+        </section>
       </section>
     </main>
   );
