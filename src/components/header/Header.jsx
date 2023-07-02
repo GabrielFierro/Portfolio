0a35ab9f50df71c7eb197172ddaef449ac4f16dc
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-02 19:34:52 -0300

    add: the header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <main>
+      <section>
+        <ul>
+          <ul>
+            <li>Github</li>
+            <li>Linkedin</li>
+          </ul>
+          <li>
+            <img src='' alt='Profile picture' />
+          </li>
+        </ul>
+
+        <h1>Hi, I&apos;m Gabriel Fierro</h1>
+        <h3>Frontend Developer</h3>
+        <p>Specialized in CSS, Javascript, and React js based in Argentina</p>
+        <button>Contact Me</button>
+      </section>
+    </main>
+  );
+}
