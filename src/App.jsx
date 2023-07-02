f6cccfe61d1fd281239b4e06021388dd582aee7e
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-02 19:37:02 -0300

    add: the header component below of the navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Header from './components/header/Header';
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
