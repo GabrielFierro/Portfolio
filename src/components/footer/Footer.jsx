a0856dbe0313c1bba9c8353c67784ea11981d845
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-03 14:06:44 -0300

    add: texts with styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,18 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className='bg-accent h-60 flex flex-col justify-between mt-12 text-lightMode text-left p-6'>
+      <div>
+        <h2>Gabriel</h2>
+        <p>Frontend Developer</p>
+      </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About Me</li>
+          <li>Projects</li>
+          <li>Contact Me</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
