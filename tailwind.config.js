9410bc22af884f851983020af819954697120350
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-03 13:37:51 -0300

    add: a color for the email

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
       accent: '#9A51E0',
       title: '#00003D',
       description: '#3F4258',
+      email: '#9E9E9E',
       lightMode: '#F3F3F3',
       darkMode: '#1A1A40'
     },
