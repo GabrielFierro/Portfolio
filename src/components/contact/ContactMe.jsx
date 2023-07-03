09a29a2a6434feb7f39265643d1201d41d671592
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-03 01:39:24 -0300

    add: the contact component with a title and his subtitle

---
+++ src/components/contact/ContactMe.jsx
@@ -0,0 +1,8 @@
+export default function ContactMe() {
+  return (
+    <section className='mt-9'>
+      <h2 className='text-3xl text-title font-semibold'>Contact Me</h2>
+      <p className='text-md text-description font-normal mt-2'>Get in touch</p>
+    </section>
+  );
+}
