2f9be15cb4837d4ddaf5dbb9da0fc123d10cbf92
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-03 01:16:07 -0300

    add: a file with data about the project image, title, and description

---
+++ src/components/projects/cardData.js
@@ -0,0 +1,57 @@
+import Loopstudios from '../../assets/images/Loopstudios.webp';
+import Magenta from '../../assets/images/magenta.webp';
+import ProductPreview from '../../assets/images/product-preview.webp';
+import Sunnyside from '../../assets/images/sunnyside.webp';
+import Travelling from '../../assets/images/travelling.webp';
+import WeatherApp from '../../assets/images/weather-app.webp';
+
+export const data = [
+  {
+    id: 1,
+    title: 'Magenta Landing Page',
+    src: Magenta,
+    alt: 'Screenshot about the Magenta website',
+    description:
+      'We are a World Wide corporation in which we teach, educate and inspire others to follow the path of Front-end development! People often ask how to become a developer, what resources would we recommend, what is the correct order for learning these technologies.'
+  },
+  {
+    id: 2,
+    title: 'Travelling Landing Page',
+    src: Travelling,
+    alt: 'Screenshot about the Travelling Landing Page',
+    description:
+      'Explore your dream place and find your next destination. We provide all necessary details for tourists who are interested in visiting beautiful destinations.'
+  },
+  {
+    id: 3,
+    title: 'Weather App',
+    src: WeatherApp,
+    alt: 'Screenshot about the Weather App',
+    description:
+      'It shows the current weather based on your location with temperature in Celsius, an icon, background personalized.'
+  },
+  {
+    id: 4,
+    title: 'Sunnyside Landing Page',
+    src: Sunnyside,
+    alt: 'Screenshot about the Sunnyside Landing Page',
+    description:
+      'We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.'
+  },
+  {
+    id: 5,
+    title: 'Loopstudios Landing Page',
+    src: Loopstudios,
+    alt: 'Screenshot about the Loopstudios Landing Page',
+    description:
+      'The leader in interactive VR. Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. '
+  },
+  {
+    id: 6,
+    title: 'Product Preview',
+    src: ProductPreview,
+    alt: 'Screenshot about the Product Preview website',
+    description:
+      'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.'
+  }
+];
