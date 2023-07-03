8649372d69864e6e9348fe62da4b7a18b1fb7c43
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-03 13:27:55 -0300

    add: the Form component

--- src/components/contact/ContactMe.jsx
+++ src/components/contact/ContactMe.jsx
@@ -1,8 +1,11 @@
+import Form from './Form';
+
 export default function ContactMe() {
   return (
     <section className='mt-9'>
       <h2 className='text-3xl text-title font-semibold'>Contact Me</h2>
       <p className='text-md text-description font-normal mt-2'>Get in touch</p>
+      <Form />
     </section>
   );
 }
---
+++ src/components/contact/Form.jsx
@@ -0,0 +1,15 @@
+export default function Form() {
+  return (
+    <section>
+      <div className='flex flex-row'>
+        <div>
+          <img src='' alt='' />
+        </div>
+        <div className='flex flex-col'>
+          <h2>Email</h2>
+          <span>fierrogabriel13@gmail.com</span>
+        </div>
+      </div>
+    </section>
+  );
+}
