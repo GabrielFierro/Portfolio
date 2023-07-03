efbe47955f7e8387a5e93c8fc145c243297ebedf
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-03 01:20:04 -0300

    add: the Project component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Header from './components/header/Header';
 import AboutMe from './components/about/AboutMe';
+import Project from './components/projects/Project';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Header />
       <AboutMe />
+      <Project />
     </>
   );
 }
