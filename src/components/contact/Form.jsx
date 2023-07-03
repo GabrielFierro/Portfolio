b5c24181f5a590ffb71f8d49729c8b750bc7af5f
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-03 14:01:15 -0300

    add: the send message button

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,4 +1,5 @@
 import EmailIcon from '../../assets/icons/mail.svg';
+import ArrowAlt from '../../assets/icons/dark-mode/arrow-alt.svg';
 
 export default function Form() {
   return (
@@ -39,6 +40,16 @@ export default function Form() {
           />
         </div>
       </form>
+      <button className='bg-accent text-lightMode font-light flex space-x-2 items-center p-3 rounded mt-8'>
+        <p>Send Message</p>
+        <img
+          className='rotate-90'
+          src={ArrowAlt}
+          alt='Arrow right'
+          width='24px'
+          height='24px'
+        />
+      </button>
     </section>
   );
 }
