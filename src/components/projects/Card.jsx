592bf32da28dc1f0ed87d0eac6b0cbdc78998a1e
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-06 19:33:07 -0300

    fix: reducing the text of the magenta project

--- src/components/projects/Card.jsx
+++ src/components/projects/Card.jsx
@@ -15,16 +15,16 @@ export default function Card() {
           alt={data.alt}
         />
         <div className='p-4'>
-          <h2 className='text-title font-semibold text-2xl py-4'>
-            {data.title}
-          </h2>
+          <h2 className='text-title font-bold text-2xl py-4'>{data.title}</h2>
           <p className='font-normal text-description'>{data.description}</p>
-          <p>Technologies used</p>
-          <ul className='flex flex-row justify-between mt-6'>
-            {data.stack.map((text) => {
-              return <li key={text}>{text}</li>;
-            })}
-          </ul>
+          <div className='flex flex-col justify-between mt-6'>
+            <p className='font-semibold text-title'>Technologies used</p>
+            <ul className='flex flex-row flex-wrap justify-around pt-4 font-normal text-title text-sm md:text-lg'>
+              {data.stack.map((text) => {
+                return <li key={text}>{text}</li>;
+              })}
+            </ul>
+          </div>
           <div className='flex flex-row space-x-2 justify-center'>
             <button className='bg-accent text-lightMode font-light flex space-x-2 items-center p-3 rounded mt-8'>
               <p className='uppercase font-normal'>Demo</p>
