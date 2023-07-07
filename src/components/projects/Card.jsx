d0885c03075eb7abbf3c8bc75e2760c1f1d77194
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-07 19:35:28 -0300

    fix: the title, buttons and the link icon

--- src/components/projects/Card.jsx
+++ src/components/projects/Card.jsx
@@ -1,5 +1,5 @@
 import ArrowAlt from '../../assets/icons/dark-mode/arrow-alt.svg';
-import LinkIcon from '../../assets/icons/link.svg';
+import LinkIcon from '../../assets/icons/dark-mode/link.svg';
 import { data } from './cardData';
 
 export default function Card() {
@@ -17,7 +17,7 @@ export default function Card() {
           height='550px'
         />
         <div className='p-4'>
-          <h2 className='text-title font-bold text-2xl py-4'>{data.title}</h2>
+          <h2 className='text-title font-bold text-2xl py-2'>{data.title}</h2>
           <p className='font-normal text-description text-xl md:px-8'>
             {data.description}
           </p>
@@ -32,21 +32,21 @@ export default function Card() {
             </ul>
           </div>
           <div className='flex flex-row space-x-2 justify-center'>
-            <button className='bg-accent text-lightMode font-light flex space-x-2 items-center p-3 rounded mt-8'>
-              <p className='uppercase font-normal'>Demo</p>
+            <button className='bg-lightMode hover:bg-lightModeHover text-accent border font-light flex space-x-2 items-center p-3 rounded mt-8'>
+              <p className='uppercase font-normal'>Repo</p>
               <img
-                className='rotate-90 text-lightMode'
-                src={ArrowAlt}
+                className='rotate-0'
+                src={LinkIcon}
                 alt='Arrow right'
                 width='24px'
                 height='24px'
               />
             </button>
-            <button className='bg-accent text-lightMode font-light flex space-x-2 items-center p-3 rounded mt-8'>
-              <p className='uppercase font-normal'>Repo</p>
+            <button className='bg-accent hover:bg-accentHover text-lightMode font-light flex space-x-2 items-center p-3 rounded mt-8'>
+              <p className='uppercase font-normal'>Demo</p>
               <img
-                className='rotate-0'
-                src={LinkIcon}
+                className='rotate-90 text-lightMode'
+                src={ArrowAlt}
                 alt='Arrow right'
                 width='24px'
                 height='24px'
