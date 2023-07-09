4ce90dc90faa1cd55106c78eb3dfc083605c3335
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-09 03:17:16 -0300

    fix: the margin and padding, change somo colors in light and dark mode

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -2,10 +2,12 @@ import Card from './Card';
 
 export default function Project() {
   return (
-    <section className='mt-16 p-8' id='projects'>
+    <section className='pt-16 px-8 bg-lightMode dark:bg-darkMode' id='projects'>
       <article className='h-full flex flex-col text-center'>
-        <h2 className='text-3xl text-title font-semibold'>Projects</h2>
-        <p className='text-md text-description font-normal mt-2'>
+        <h2 className='text-3xl text-title dark:text-lightMode font-semibold'>
+          Projects
+        </h2>
+        <p className='text-md text-description dark:text-gray font-normal mt-2'>
           Most popular work
         </p>
         <Card />
