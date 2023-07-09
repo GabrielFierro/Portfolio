d27ee3b8c089e39c6423ab937ee3b2e5e4bc850f
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-09 03:12:35 -0300

    add: colors for dark mode and change the email color

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -9,8 +9,10 @@ export default function Form() {
           <img src={EmailIcon} alt='Email icon' width='50px' height='50px' />
         </div>
         <div className='flex flex-col text-left px-4'>
-          <h2 className='text-3xl text-title font-normal'>Email</h2>
-          <span className='text-sm text-email font-light'>
+          <h2 className='text-3xl text-title dark:text-lightMode font-normal'>
+            Email
+          </h2>
+          <span className='text-sm text-gray font-light'>
             fierrogabriel13@gmail.com
           </span>
         </div>
@@ -22,21 +24,21 @@ export default function Form() {
             id='name'
             name='name'
             placeholder='Name'
-            className='bg-accent bg-opacity-20 placeholder-black text-sm font-light py-6 px-5 rounded-md w-full'
+            className='bg-accent bg-opacity-20 dark:bg-accentDark placeholder-black text-sm font-light py-6 px-5 rounded-md w-full'
           />
           <input
             type='text'
             id='email'
             name='email'
             placeholder='Email'
-            className='bg-accent bg-opacity-20 placeholder-black text-sm font-light py-6 px-5 rounded-md w-full'
+            className='bg-accent bg-opacity-20 dark:bg-accentDark placeholder-black text-sm font-light py-6 px-5 rounded-md w-full'
           />
           <textarea
             type='message'
             id='message'
             name='message'
             placeholder='Message'
-            className='bg-accent bg-opacity-20 placeholder-black text-sm font-light py-12 px-5 rounded-md w-full'
+            className='bg-accent bg-opacity-20 dark:bg-accentDark placeholder-black text-sm font-light py-12 px-5 rounded-md w-full'
           />
         </div>
       </form>
