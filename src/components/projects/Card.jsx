99a1429a92d5ae162bdcfe7deec26a796e9f9707
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-09 03:29:44 -0300

    fix: colors for light and dark mode, position and rounded

--- src/components/projects/Card.jsx
+++ src/components/projects/Card.jsx
@@ -6,35 +6,35 @@ export default function Card() {
   const card = data.map((data) => {
     return (
       <div
-        className='shadow-md shadow-lightMode border border-title rounded-lg max-w-sm md:max-w-screen-lg mt-12 md:flex'
+        className='bg-cardLight dark:bg-card drop-shadow-md dark:drop-shadow-lg z-0 dark:z-0 rounded-lg max-w-sm md:max-w-screen-lg mt-12 md:flex'
         key={data.id}
       >
         <img
-          className='rounded-t-lg md:rounded-none cursor-pointer base:h-56 sm:h-72 md:h-full'
+          className='rounded-t-lg md:rounded-none xl:rounded-md base:h-56 sm:h-72 md:h-full'
           src={data.src}
           alt={data.alt}
           width='400px'
           height='550px'
         />
         <div className='p-4'>
-          <h2 className='text-title font-bold text-xl lg:text-2xl py-2'>
+          <h2 className='text-title dark:text-lightMode font-bold text-xl lg:text-2xl py-2'>
             {data.title}
           </h2>
-          <p className='font-normal text-description text-md lg:text-xl px-2 lg:px-8'>
+          <p className='font-normal text-description dark:text-gray text-md lg:text-xl px-2 lg:px-8'>
             {data.description}
           </p>
           <div className='flex flex-col justify-between mt-6'>
-            <p className='font-semibold text-title text-md lg:text-lg'>
+            <p className='font-semibold text-title dark:text-lightMode text-md lg:text-lg'>
               Developed with
             </p>
-            <ul className='flex flex-row flex-wrap justify-around md:justify-evenly pt-4 base:mx-6 md:mx-0 font-normal text-title text-md lg:text-lg'>
+            <ul className='flex flex-row flex-wrap justify-around md:justify-evenly pt-4 base:mx-6 md:mx-0 font-normal text-title dark:text-gray text-md lg:text-lg'>
               {data.stack.map((text) => {
                 return <li key={text}>{text}</li>;
               })}
             </ul>
           </div>
           <div className='flex flex-row space-x-2 justify-center'>
-            <button className='bg-lightMode hover:bg-lightModeHover text-accent border font-light flex space-x-2 items-center p-3 rounded mt-8'>
+            <button className='bg-lightMode hover:bg-lightModeHover dark:bg-darkMode dark:hover:bg-card text-accent border font-light flex space-x-2 items-center p-3 rounded mt-8'>
               <p className='uppercase font-normal'>Repo</p>
               <img
                 src={LinkIcon}
