bef4ce1a657d6fce578f25ce4c95f36c978c2f85
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-09 03:05:22 -0300

    add: the darkMode declaration, add new colores, and change some

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: 'class',
   theme: {
     fontFamily: {
       rubik: ['rubik', 'serif']
     },
     colors: {
       accent: '#9A51E0',
+      accentDark: '#C49AEB',
       accentHover: '#8531D3',
+      accentHoverDark: '#D0ACF1',
       title: '#00003D',
       description: '#3F4258',
-      email: '#9E9E9E',
+      descriptionDark: '#A09BAB',
+      gray: '#9E9E9E',
       black: '#000000',
+      card: '#1F2127',
       lightMode: '#F3F3F3',
       lightModeHover: '#EEEEEE',
-      darkMode: '#1A1A40',
+      darkMode: '#1A1C22',
       darkModeHover: '#111139'
     },
     extend: {}
