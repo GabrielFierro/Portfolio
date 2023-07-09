0c833e2fa8c7868fb9d2d6a12704cc99b331f8a8
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-09 03:13:32 -0300

    fix: the padding, margin and change some colors

--- src/components/contact/ContactMe.jsx
+++ src/components/contact/ContactMe.jsx
@@ -2,11 +2,14 @@ import Form from './Form';
 
 export default function ContactMe() {
   return (
-    <section className='mt-9 p-8' id='contact'>
-      <h2 className='text-center text-3xl text-title font-semibold'>
+    <section
+      className='bg-lightMode dark:bg-darkMode pt-9 lg:pt-16 px-8 pb-12'
+      id='contact'
+    >
+      <h2 className='text-center text-3xl text-title dark:text-lightMode font-semibold'>
         Contact Me
       </h2>
-      <p className='text-center text-md text-description font-normal mt-2'>
+      <p className='text-center text-md text-description dark:text-gray font-normal mt-2'>
         Get in touch
       </p>
       <Form />
