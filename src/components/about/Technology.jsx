00dd2cfe5b663a8e9968515be35bb99f8b1cf4e7
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-09 03:09:50 -0300

    add: a color for each theme, light and dark mode

--- src/components/about/Technology.jsx
+++ src/components/about/Technology.jsx
@@ -3,7 +3,7 @@ export default function Technology({ src, alt, name }) {
   return (
     <li className='flex flex-row space-x-3 md:space-x-2 px-4 mt-3'>
       <img src={src} alt={alt} width='24px' height='24px' />
-      <p>{name}</p>
+      <p className='text-title dark:text-gray'>{name}</p>
     </li>
   );
 }
