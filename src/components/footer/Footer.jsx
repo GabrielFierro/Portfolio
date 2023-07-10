2608a3618d065afa68817d565cbbfc780beee5ad
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-10 16:37:03 -0300

    add: links to each section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-scroll';
 export default function Footer() {
   return (
     <div className='bg-accent h-72 w-full flex flex-col justify-between text-lightMode text-left p-8'>
@@ -8,11 +9,47 @@ export default function Footer() {
         </p>
       </div>
       <div>
-        <ul className='py-6 space-y-2'>
-          <li>Home</li>
-          <li>About Me</li>
-          <li>Projects</li>
-          <li>Contact Me</li>
+        <ul>
+          <Link
+            href='#home'
+            spy={true}
+            to='home'
+            smooth={true}
+            offset={-60}
+            duration={900}
+          >
+            <li className='pb-1'>Home</li>
+          </Link>
+          <Link
+            href='#about'
+            spy={true}
+            to='about'
+            smooth={true}
+            offset={-60}
+            duration={900}
+          >
+            <li className='py-1'>About Me</li>
+          </Link>
+          <Link
+            href='#projects'
+            spy={true}
+            to='projects'
+            smooth={true}
+            offset={-60}
+            duration={900}
+          >
+            <li className='py-1'>Projects</li>
+          </Link>
+          <Link
+            href='#contact'
+            spy={true}
+            to='contact'
+            smooth={true}
+            offset={-60}
+            duration={900}
+          >
+            <li className='pt-1'>Contact Me</li>
+          </Link>
         </ul>
       </div>
     </div>
