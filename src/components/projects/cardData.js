826f632691ef97e24240957a8f8074a228a652cd
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-11 10:21:18 -0300

    fix: change the id, the title and code for repo name

--- src/components/projects/cardData.js
+++ src/components/projects/cardData.js
@@ -7,69 +7,69 @@ import WeatherApp from '../../assets/images/weather-app.webp';
 
 export const data = [
   {
-    id: 1,
-    title: 'Magenta Landing Page',
+    id: 0,
+    title: 'Magenta',
     src: Magenta,
     alt: 'Screenshot about the Magenta website',
     description:
       'We are a World Wide corporation in which we teach, educate and inspire others to follow the path of Front-end development!',
     stack: ['HTML', 'CSS', 'Javascript', 'Tailwind', 'Git'],
-    repo: 'https://github.com/magentateam/landingOng',
+    code: 'https://github.com/magentateam/landingOng',
     demo: 'https://magentateam.github.io/landingOng/public/'
   },
   {
-    id: 2,
-    title: 'Sunnyside Landing Page',
+    id: 1,
+    title: 'Sunnyside',
     src: Sunnyside,
     alt: 'Screenshot about the Sunnyside Landing Page',
     description:
-      'We are a full-service creative agency specializing in helping brands grow fast with marketing for you.',
+      'We are a full-service creative agency specializing in helping brands grow fast with the perfect marketing for you.',
     stack: ['HTML', 'CSS', 'Javascript', 'React', 'Git'],
-    repo: 'https://github.com/GabrielFierro/Sunnyside-agency-landing-page',
+    code: 'https://github.com/GabrielFierro/Sunnyside-agency-landing-page',
     demo: 'https://resonant-kringle-6016a6.netlify.app'
   },
   {
-    id: 3,
+    id: 2,
     title: 'Weather App',
     src: WeatherApp,
     alt: 'Screenshot about the Weather App',
     description:
       'It shows the current weather based on your location with temperature in Celsius, an icon, and personalized background.',
     stack: ['HTML', 'CSS', 'Javascript', 'API', 'Git'],
-    repo: 'https://github.com/GabrielFierro/Weather-app',
+    code: 'https://github.com/GabrielFierro/Weather-app',
     demo: 'https://weather-app-taupe-rho.vercel.app'
   },
   {
-    id: 4,
+    id: 3,
     title: 'Travelling Landing Page',
     src: Travelling,
     alt: 'Screenshot about the Travelling Landing Page',
     description:
       'Explore your dream place and find your next destination. Join us and discover beautiful destinations.',
     stack: ['HTML', 'CSS', 'Javascript', 'React', 'Chakra UI', 'Git'],
-    repo: 'https://github.com/GabrielFierro/Travelling-landing-page',
+    code: 'https://github.com/GabrielFierro/Travelling-landing-page',
     demo: 'https://travelling-landing-page.vercel.app'
   },
   {
-    id: 5,
-    title: 'Loopstudios Landing Page',
+    id: 4,
+    title: 'Loopstudios',
     src: Loopstudios,
     alt: 'Screenshot about the Loopstudios Landing Page',
     description:
       'The leader in interactive VR. Loopstudios has been producing world-class virtual reality projects for greatest companies.',
     stack: ['HTML', 'CSS', 'Javascript', 'Git'],
-    repo: 'https://github.com/GabrielFierro/loopstudios-landing-page',
+    code: 'https://github.com/GabrielFierro/loopstudios-landing-page',
     demo: 'https://loopstudios-landing-page-jade.vercel.app'
   },
   {
-    id: 6,
+    id: 5,
     title: 'Product Preview',
     src: ProductPreview,
     alt: 'Screenshot about the Product Preview website',
     description:
       'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.',
     stack: ['HTML', 'CSS', 'Git'],
-    repo: 'https://github.com/GabrielFierro/3-column-preview-card',
+    code: 'https://github.com/GabrielFierro/3-column-preview-card',
     demo: 'https://3-column-preview-card-prueba.netlify.app'
   }
 ];
