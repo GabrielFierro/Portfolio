e3c8d4e714651e59deade30c654398a8f2a66a2b
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-11 11:26:18 -0300

    fix: the position of the elements, his margin, and padding

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,51 +10,21 @@ export default function Header() {
 
   return (
     <main
-      className='bg-lightMode dark:bg-darkMode mt-12 md:mt-4 lg:mt-0 py-8 h-screen xs:px-28 sm:px-0 lg:px-32 flex md:items-center'
+      className='bg-lightMode dark:bg-darkMode mt-4 pt-16 lg:mt-16 h-auto xs:px-28 sm:px-0 lg:px-32 flex md:items-center justify-center pb-12'
       id='home'
     >
       <section className='flex flex-col lg:h-4/6 sm:justify-center h-auto'>
-        <div className='flex flex-row flex-wrap md:flex-nowrap justify-evenly md:justify-center'>
-          <ul className=' flex flex-col basis-32 justify-center items-center pl-16'>
-            <li className='flex my-2 cursor-pointer'>
-              <a
-                href='https://github.com/GabrielFierro/'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <img
-                  src={GithubIcon}
-                  alt='Github icon'
-                  width={('24px', '32px')}
-                  height={('24px', '32px')}
-                />
-              </a>
-            </li>
-            <a
-              href='https://www.linkedin.com/in/gabriel-fierro-2020/'
-              target='_blank'
-              rel='noreferrer'
-            >
-              <li className='flex my-2 cursor-pointer'>
-                <img
-                  src={LinkedInIcon}
-                  alt='Linkedin icon'
-                  width={('24px', '32px')}
-                  height={('24px', '32px')}
-                />
-              </li>
-            </a>
-          </ul>
-          <li className='flex lg:justify-center flex-1 order-1 md:order-3 md:items-center'>
+        <article className='flex flex-row flex-wrap justify-evenly md:justify-center'>
+          <div className='flex justify-center flex-1 order-1 md:order-3 md:items-center basis-1/3'>
             <img
-              className='w-72 md:w-56 lg:w-72 xl:w-72 md:h-52 lg:h-58 xl:h-72 -mt-0 md:mt-4 lg:-ml-12'
+              className='w-72 md:w-64 lg:w-72 md:h-60 lg:h-72 -mt-0 md:-mt-8 lg:-ml-12'
               src={ProfilePicture}
               alt='Profile picture'
               width='250px'
               height='250px'
             />
-          </li>
-          <section className='flex flex-col basis-full md:basis-7/12 order-2 md:order-1 text-left w-auto mt-0 md:mt-12 justify-center md:items-stretch p-8'>
+          </div>
+          <section className='bg-lightMode dark:bg-darkMode flex flex-col basis-full md:basis-7/12 order-2 md:order-1 text-left w-auto mt-0 md:mt-20 justify-center md:items-stretch px-8'>
             <h1 className='text-title dark:text-lightMode text-2xl lg:text-5xl font-bold pb-1'>
               {t('header.title')}
             </h1>
@@ -64,6 +34,36 @@ export default function Header() {
             <p className='text-description dark:text-gray text-md font-normal pb-4'>
               {t('header.description')}
             </p>
+            <ul className='flex flex-row justify-start items-center py-4 space-x-2 order-1'>
+              <li className='flex my-2 cursor-pointer'>
+                <a
+                  href='https://github.com/GabrielFierro/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <img
+                    src={GithubIcon}
+                    alt='Github icon'
+                    width={('24px', '32px')}
+                    height={('24px', '32px')}
+                  />
+                </a>
+              </li>
+              <a
+                href='https://www.linkedin.com/in/gabriel-fierro-2020/'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <li className='flex my-2 cursor-pointer'>
+                  <img
+                    src={LinkedInIcon}
+                    alt='Linkedin icon'
+                    width={('24px', '32px')}
+                    height={('24px', '32px')}
+                  />
+                </li>
+              </a>
+            </ul>
             <Link
               href='#contact'
               spy={true}
@@ -83,7 +83,7 @@ export default function Header() {
               </button>
             </Link>
           </section>
-        </div>
+        </article>
       </section>
     </main>
   );
