e4d778fdf290ecb28763f9729cee2a57d954b6bb
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-11 11:33:35 -0300

    add: margin top to middle and above devices

--- src/components/contact/ContactMe.jsx
+++ src/components/contact/ContactMe.jsx
@@ -5,7 +5,7 @@ export default function ContactMe() {
   const [t, i18n] = useTranslation('global');
   return (
     <section
-      className='bg-lightMode dark:bg-darkMode pt-9 lg:pt-16 px-8 pb-12'
+      className='bg-lightMode dark:bg-darkMode pt-9 lg:pt-16 px-8 pb-12 md:-mt-2'
       id='contact'
     >
       <h2 className='text-center text-3xl text-title dark:text-lightMode font-semibold'>
