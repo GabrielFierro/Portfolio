7c364c98436e46687d09f4c80f46f00c60f5de32
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-20 17:21:02 -0300

    add: the build property, viteCompression and imagemin plugins

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import imagemin from 'vite-plugin-imagemin';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: true // Habilita la minificación de los archivos
+  },
   plugins: [
+    viteCompression(),
+    imagemin({
+      webp: {
+        quality: 50,
+        buffer: true
+      }
+    }),
     react(),
     svgr({
       svgrOptions: {
