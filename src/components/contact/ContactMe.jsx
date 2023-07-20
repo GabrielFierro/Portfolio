3cd17c427e19d345d0d7b229806d88263e6303d8
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-07-20 17:34:05 -0300

    add: the email icon and the section previous to the Form component

--- src/components/contact/ContactMe.jsx
+++ src/components/contact/ContactMe.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 import { lazy } from 'react';
-// import Form from './Form';
 const Form = lazy(() => import('./Form'));
 import { useTranslation } from 'react-i18next';
+import EmailIcon from '../../assets/icons/mail.svg';
 
 export default function ContactMe() {
   const [t, i18n] = useTranslation('global');
@@ -17,7 +17,22 @@ export default function ContactMe() {
       <p className='text-center text-md text-description dark:text-subtitle font-normal mt-2'>
         {t('contact.subtitle')}
       </p>
-      <Form />
+      <section className='mt-12'>
+        <div className='flex flex-row'>
+          <div className='pt-1'>
+            <img src={EmailIcon} alt='Email icon' width='50px' height='50px' />
+          </div>
+          <div className='flex flex-col text-left px-4'>
+            <h2 className='text-3xl text-title dark:text-lightMode font-normal'>
+              Email
+            </h2>
+            <span className='text-sm text-description font-light'>
+              fierrogabriel13@gmail.com
+            </span>
+          </div>
+        </div>
+        <Form />
+      </section>
     </section>
   );
 }
